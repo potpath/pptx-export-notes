5932f9e1e15f17ce46e74c110ade2845f3eb71fa
Mana <mana_ohm@hotmail.com>
2015-09-24 14:09:30 +0700

    Add support for Python3 and pptx generated from LibreOffice/Google Slides.

--- pptx-export-notes.py
+++ pptx-export-notes.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 #############################################################################
 #############################################################################
 ###                                                                       ###
@@ -7,70 +8,71 @@
 #############################################################################
 #############################################################################
 
-#!/usr/bin/env python
-import argparse, os, glob
+# !/usr/bin/env python
+import argparse
+import os
+import glob
+import shutil
+import codecs
 from zipfile import ZipFile
 from xml.dom.minidom import parse
 
+TMP_PATH = '/tmp/pptx-export-notes/'
+SLIDE_DELIMITER = u'\n────────────────────────────────────────\n'
 
-#main function
+
+# main function
 def run():
     parser = argparse.ArgumentParser(description='exports speaker notes from pptx files by parsing the XML')
     parser.add_argument('-v', action='version', version='%(prog)s 1.0')
     parser.add_argument('-p', metavar='<path/to/pptx/file>', help='path to the Powerpoint 2007+ file', action='store',
-                        type=file, dest='pptxfile')
-    #add more arguments here in future if you wish to expand
+                        type=argparse.FileType('rb'), dest='pptxfile')
+    # add more arguments here in future if you wish to expand
     args = parser.parse_args()
-    #extract the pptx file as a zip archive
-    #note: only extract from pptx files that you trust. they could potentially overwrite your important files.
-    ZipFile(args.pptxfile).extractall(path='/tmp/', pwd=None)
-    path = '/tmp/ppt/notesSlides/'
+    # extract the pptx file as a zip archive
+    # note: only extract from pptx files that you trust. they could potentially overwrite your important files.
+    shutil.rmtree(TMP_PATH, ignore_errors=True)
+    ZipFile(args.pptxfile).extractall(path=TMP_PATH, pwd=None)
+    path = TMP_PATH + 'ppt/notesSlides/'
 
     notesDict = {}
-    #open up the file that you wish to write to
+    # open up the file that you wish to write to
     writepath = os.path.dirname(args.pptxfile.name) + '/' + os.path.basename(args.pptxfile.name).rsplit('.', 1)[
         0] + '_presenter_notes.txt'
-    print writepath
-    f = open(writepath, 'w')
+    print(writepath)
+    f = codecs.open(writepath, mode='w', encoding='utf-8')
+    # f = open(writepath, 'w')
 
     for infile in glob.glob(os.path.join(path, '*.xml')):
-        #parse each XML notes file from the notes folder.
+        # parse each XML notes file from the notes folder.
         dom = parse(infile)
         noteslist = dom.getElementsByTagName('a:t')
-        if len(noteslist) == 0:
+        if not noteslist:
             continue
 
-        #separate last element of noteslist for use as the slide marking.
-        slideNumber = noteslist.pop()
-        slideNumber = slideNumber.toxml().replace('<a:t>', '').replace('</a:t>', '')
-        #start with this empty string to build the presenter note itself
-        tempstring = ''
+        if noteslist[-1].parentNode.getAttribute('type') == 'slidenum':
+            # separate last element of noteslist for use as the slide marking.
+            slideNumber = noteslist.pop()
+            slideNumber = slideNumber.firstChild.nodeValue
+        else:
+            # fallback for pptx generated from LibreOffice/Google Slides
+            slideNumber = os.path.basename(infile).rsplit('.', 1)[0][len('notesSlide'):]
 
-        for node in noteslist:
-            xmlTag = node.toxml()
-            xmlData = xmlTag.replace('<a:t>', '').replace('</a:t>', '')
-            #concatenate the xmlData to the tempstring for the particular slideNumber index.
-            tempstring = tempstring + xmlData
+        # start with this empty string to build the presenter note itself
+        tempstring = '\n'.join(node.firstChild.nodeValue if node.firstChild else '' for node in noteslist)
 
-        #store the tempstring in the dictionary under the slide number
+        # store the tempstring in the dictionary under the slide number
         notesDict[slideNumber] = tempstring
 
-    #print/write the dictionary to file in sorted order by key value.
+    # print/write the dictionary to file in sorted order by key value.
     for x in [key for key in sorted(notesDict.keys(), key=int)]:
         f.write('Slide ' + str(x) + '\n')
-        stringybean = notesDict[str(x)]
-        f.write(stringybean.encode('ascii', 'ignore') + '\n')
+        f.write(notesDict[str(x)])
+        f.write(SLIDE_DELIMITER)
 
     f.close()
-    print 'file successfully written to' + '\'' + writepath + '\''
+    print('file successfully written to' + '\'' + writepath + '\'')
 
 
 if __name__ == "__main__":
-    try:
-        run()
-    except (KeyboardInterrupt, SystemExit):
-        raise
-    except:
-        print'something wrong happened'
-        # report error and proceed
-
+    run()
